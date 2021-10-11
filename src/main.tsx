297b3bbeb3b71095713be1a0816e72b86bb3a115
thangnd72 <nguyendinhthang99bn@gmail.com>
2021-10-11 07:49:02 +0700

    config store

---
+++ src/main.tsx
@@ -0,0 +1,32 @@
+import {
+  DefaultTheme,
+  NavigationContainer,
+  useNavigation,
+} from '@react-navigation/native';
+import React, {useEffect, useRef} from 'react';
+import {SafeAreaView, StatusBar} from 'react-native';
+import {enableScreens} from 'react-native-screens';
+import {Provider} from 'react-redux';
+import SwitchScreen from 'screens/SwitchScreen';
+import store from '@store/configureStore';
+const navTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: '#FFF',
+  },
+};
+enableScreens();
+const AppLayout = () => {
+  return (
+    <Provider store={store}>
+      {/* <SafeAreaView style={{ flex: 0, backgroundColor: 'white' }} />
+    <StatusBar backgroundColor={'transparent'} barStyle={'dark-content'} translucent /> */}
+      <NavigationContainer theme={navTheme}>
+        <SwitchScreen />
+      </NavigationContainer>
+    </Provider>
+  );
+};
+let App = AppLayout;
+export default App;
